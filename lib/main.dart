eeb4f6b83164e9f401f6d5b7b55b4c3bf4b2a8eb
Takumi.Igarashi <i.takumi621@gmail.com>
2022-06-09 02:57:48 +0900

    Column実装

--- lib/main.dart
+++ lib/main.dart
@@ -33,12 +33,12 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> titleList = ['amazon', '楽天', 'Yahoo!', 'みずほ銀行', 'セゾン'];
-  int _counter = 0;
+  //List<String> titleList = ['amazon', '楽天', 'Yahoo!', 'みずほ銀行', 'セゾン'];
+  //int _counter = 0;
 
   void _incrementCounter() {
     setState(() {
-      _counter++;
+      //_counter++;
     });
   }
 
@@ -46,6 +46,7 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
 
     return Scaffold(
+      backgroundColor: Colors.yellow[200],
       appBar: AppBar(
         //centerTitleをtrueでタイトル文字を中央寄せ
         centerTitle: true,
@@ -54,6 +55,58 @@ class _MyHomePageState extends State<MyHomePage> {
 
       //https://codezine.jp/article/detail/14711
       body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.only(top: 100)),
+                Container(
+                    alignment: Alignment.center,
+                    child: const Text(
+                      'カウンター',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontStyle: FontStyle.normal,
+                      ),
+                    ),
+                    //color: Colors.blue,
+                    width: 130,
+                    height:50,
+                    decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white),
+                    borderRadius: BorderRadius.circular(70),
+                    color: Colors.red[200],
+                ),
+              ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),),
+                Container( color: Colors.red, width: 100, height:100 ),
+              ],
+            ),
+
+            const Padding(padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),),
+
+            Column(
+              children: <Widget>[
+                const Padding(padding: EdgeInsets.only(top: 100)),
+                Container( color: Colors.green, width: 100, height:100 ),
+
+                const Padding(padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),),
+                Container( color: Colors.orange, width: 100, height:100 ),
+              ],
+            ),
+
+          ],
+        ),
+      ),
+
+    );
+  }
+}
+
+
+      /*body: Center(
         child: Column(
           //crossAxisAlignment: CrossAxisAlignment.start,
           children:[
@@ -62,34 +115,44 @@ class _MyHomePageState extends State<MyHomePage> {
               child: Column(//Columnウィジェットのchildrenプロパティでウィジェットを縦に表示する
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
+                  //Containerの重ね方
+                  //https://www.choge-blog.com/programming/fluttercontainer-overlay/
                   Stack(
                     children: [
                       Align(
                         alignment: Alignment.center,
-                          child: Container(
-                            width: 330,
-                            height: 280,
-                            decoration: BoxDecoration(
-                              border: Border.all(color: Colors.white),
-                              borderRadius: BorderRadius.circular(70),
-                              color: Colors.blue[200],
-                              ),
-                            ),
-                      ),
-                      Align(
-                        alignment: Alignment.center,
-                          child: Container(
-                        width: 130,
-                        height: 50,
-                        decoration: BoxDecoration(
-                          border: Border.all(color: Colors.white),
-                          borderRadius: BorderRadius.circular(70),
-                          color: Colors.red[200],
+                        //大枠の青コンテナ
+                        child: Container(
+                          width: 330,
+                          height: 280,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(70),
+                            color: Colors.blue[200],
+                          ),
                         ),
                       ),
+                      Align(
+                        //中の小さい赤コンテナ
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: const Text(
+                            'カウンター',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontStyle: FontStyle.normal,
+                            ),
+                          ),
+                          width: 130,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white),
+                            borderRadius: BorderRadius.circular(70),
+                            color: Colors.red[200],
+                          ),
+                        ),
                       ),
                     ],
-
                   ),
                   //余白を設ける
                   Padding(padding: EdgeInsets.all(10)),
@@ -99,10 +162,8 @@ class _MyHomePageState extends State<MyHomePage> {
             Padding(padding: EdgeInsets.all(10)),
           ],
         ),
-      ),
-    );
-  }
-}
+      ),*/
+
 
 
 //Memo 頭大文字はウィジェット
